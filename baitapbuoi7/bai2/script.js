4ce01afbdcc6c1baba6a826951c6f9181bba41a2
nguyen van xuan <nguyenvanxuan30082003@gmail.com>
2022-10-26 22:38:52 +0700

    ten commit

---
+++ baitapbuoi7/bai2/script.js
@@ -0,0 +1,8 @@
+var a;
+a = prompt("Nhap vao so a", "0");
+a = parseInt(a);
+if (a % 2 == 0) {
+  document.write("a la so chan");
+} else {
+  document.write("a la so le");
+}
---
+++ baitapbuoi7/bai3/script.js
@@ -0,0 +1,8 @@
+var a;
+a = prompt("Nhap vao so a = ", "0");
+a = parseInt(a);
+if (a == 0) {
+  document.write("da doan dung");
+} else {
+  document.write("ban doan sai vui long nhap lai so");
+}
